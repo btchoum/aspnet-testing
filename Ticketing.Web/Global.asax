2c6cba90882aab1b681de494a9c0a1cfb507e8ad
btchoum <bertrand.tchoumkeu@gmail.com>
2019-11-17 15:37:20 -0600

    create web app and integration tests projects

---
+++ Ticketing.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ticketing.Web.Global" Language="C#" %>
---
+++ src/Ticketing.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Ticketing.IntegrationTests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
